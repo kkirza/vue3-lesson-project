3bc8ce890164dbce7ce811ac70d2019e7379aed4
kkirza <zasuhink2017@gmail.com>
2022-08-25 02:41:13 +0700

    change

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
-    <post-list :posts="posts" @remove="removePost" />
+    <h1>Страница с постами</h1>
+    <div class="app__btns">
+      <my-button @click="showDialog"> Создать пост </my-button>
+      <my-select v-model="selectedSort" :options="sortOptions"> </my-select>
+    </div>
+
+    <my-dialog v-model:show="dialogVisable">
+      <post-form @create="createPost" />
+    </my-dialog>
+    <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostsLoading" />
+    <div v-else>Идёт загрузка</div>
   </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import axios from "axios";
 export default {
   components: {
     PostList,
@@ -15,24 +25,65 @@ export default {
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: "JavaScript", body: "Описание поста" },
-        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
-        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
-        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
-        { id: 5, title: "JavaScript 5", body: "Описание поста 5" },
+      posts: [],
+      dialogVisable: false,
+      isPostsLoading: false,
+      selectedSort: "",
+      sortOptions: [
+        {
+          value: "title",
+          name: "По названию",
+        },
+        {
+          value: "body",
+          name: "По описанию",
+        },
       ],
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisable = false;
+    },
+    showDialog() {
+      this.dialogVisable = true;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
     },
+    async fetchPostst() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert("Ошибка");
+      } finally {
+        this.isPostsLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPostst();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1,post2) =>{
+        return post1[this.selectedSort] ?.localeCompare(post2[this.selectedSort])
+      })
+    },
+  },
+  watch: {
   },
 };
 </script>
 
-<style></style>
+<style>
+.app__btns {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
